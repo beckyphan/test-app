56f56e0c74209c82d63102dcfbabe9fce10b01d0
beckyphan <github email address>
2020-11-18 22:47:30 -0800

    added starter index.css code for tic-tac-toe

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {Router} from 'react-router';
 import './index.css';
 import App from './App';
 
+const store = createStore()
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
