e23f4c022d13a8300f4ff4d36c5a11144de73ca0
beckyphan <github email address>
2020-11-21 16:57:34 -0800

    allow each click to toggle xIsNext state

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ class Board extends React.Component {
     const squares = this.state.squares.slice()
     squares[i] = 'X'
     this.setState({
-      squares: squares
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
     })
   }
 
